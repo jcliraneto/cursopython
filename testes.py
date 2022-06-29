7601cbd18e67ec2069ffdd71b7a797c7ce4e5440
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-06-29 16:35:59 -0300

    atualização do codigo para implementação das variáveis dif_idaden e dif_idadev

--- testes.py
+++ testes.py
@@ -1,18 +1,19 @@
 print("***Comparador de idades mk1***")
 sua_idadestr = input("Digite sua idade: ")
 comp_idadestr = input("Digite a idade da outra pessoa: ")
-
 sua_idade = int(sua_idadestr)
 comp_idade = int(comp_idadestr)
 idadesiguais = sua_idade == comp_idade
 maisnovo = sua_idade < comp_idade
 maisvelho = sua_idade > comp_idade
+dif_idaden = comp_idade - sua_idade
+dif_idadev = sua_idade - comp_idade
 
 if(idadesiguais):
     print("Vocês tem a mesma idade!")
 else:
     if(maisnovo):
-        print("Você é mais novo que a outra pessoa")
+        print("Você é", dif_idaden, "anos mais novo que a outra pessoa")
     elif (maisvelho):
-        print("Você é mais velho que a outra pessoa")
+        print("Você é", dif_idadev, "anos mais velho que a outra pessoa")
 
