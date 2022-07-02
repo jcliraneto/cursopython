55ede88029bf0a0f9ba933bf13965c9853985972
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-07-02 17:36:11 -0300

    implementação do while para repetição do bloco de comandos

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,20 +3,26 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite seu numero: "))
-print("você digitou ", chute)
-acertou    = chute == numero_secreto
-chutemaior = chute > numero_secreto
-chutemenor = chute < numero_secreto
+while(rodada <= total_de_tentativas):
+    print("tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite seu numero: "))
+    print("você digitou ", chute)
 
-if(acertou):
-    print("Você Acertou!")
-else:
-    if(chutemaior):
-        print("você Errou! O seu chute foi maior que o numero secreto.")
-    elif(chutemenor):
-        print("você Errou! O seu chute foi menor que o numero secreto.")
+    acertou    = chute == numero_secreto
+    chutemaior = chute > numero_secreto
+    chutemenor = chute < numero_secreto
 
-print("Fim do jogo!")
+    if(acertou):
+        print("Você Acertou! Parabéns!!")
+    else:
+        if(chutemaior):
+            print("você Errou! O seu chute foi maior que o numero secreto.")
+        elif(chutemenor):
+            print("você Errou! O seu chute foi menor que o numero secreto.")
+    rodada = rodada + 1
+
+print("Não fo dessa vez... fim do jogo!")
 
