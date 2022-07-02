f3028bfc8eaf2d4a975ff04e8806f556739c28d2
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-07-02 18:17:03 -0300

    otimizando o codigo com a função "for" e colocação das funções "continue" e "break"

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,26 +3,30 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
-total_de_tentativas = 3
+total_de_tentativas = 5
 rodada = 1
 
-while(rodada <= total_de_tentativas):
+for rodada in range (1, total_de_tentativas + 1):
     print("tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = int(input("Digite seu numero: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
     print("você digitou ", chute)
 
-    acertou    = chute == numero_secreto
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100 ")
+        continue
+
+    acertou = chute == numero_secreto
     chutemaior = chute > numero_secreto
     chutemenor = chute < numero_secreto
 
     if(acertou):
         print("Você Acertou! Parabéns!!")
+        break
     else:
         if(chutemaior):
             print("você Errou! O seu chute foi maior que o numero secreto.")
         elif(chutemenor):
             print("você Errou! O seu chute foi menor que o numero secreto.")
-    rodada = rodada + 1
 
-print("Não fo dessa vez... fim do jogo!")
+print("fim do jogo!")
 
