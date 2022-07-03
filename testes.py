e813d2e7e0bc5af3842bc3b6ead1fd0db10bf3e1
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-07-03 12:21:38 -0300

    alteração nos exemplos do curso

--- testes.py
+++ testes.py
@@ -1,10 +1,3 @@
-idade_str = input("Digite sua idade: ")
-idade = int(idade_str)
-
-if (idade >= 18):
-    print("Você é maior de idade.")
-else:
-    if (idade <= 12):
-        print("Você é uma criança.")
-    elif (idade > 12):
-        print("Você é um adolescente.")
+contador = 1
+for contador in range (1,45,11):
+    print(contador)
