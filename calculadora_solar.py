60ac89c6014300edf29acafe2b5ee2f19bfc8f69
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-07-03 11:02:57 -0300

    primeira logica da programação

--- calculadora_solar.py
+++ calculadora_solar.py
@@ -1,7 +1,12 @@
 print("*****Calculadora solar mk1*****")
 
-KWH = int(input("Digite a quantidade de kWh: "))
-indicesolar = int(input("Digite o indice solar: "))
-KWP = float(KWH / indicesolar)
+calcular = (input("Digite a unidade que deseja calcular. (KWH ou KWP): "))
 
-print("{}kWh corresponde a {:.3}kWp".format(KWH, KWP))
+if (calcular = KWH):
+
+    KWH = int(input("Digite a quantidade de kWh: "))
+    indicesolar = int(input("Digite o indice de irradiação solar: "))
+    KWP = float(KWH / indicesolar)
+
+    print("{}kWh corresponde a {:.3}kWp".format(KWH, KWP))
+    print("Fim do programa!")
