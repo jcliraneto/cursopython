f26042b76950648fd1d04c6e81494a3e78a61802
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-08-25 17:35:04 -0300

    preparação do hub de jogos.py

--- jogos/jogos.py
+++ jogos/jogos.py
@@ -1,3 +1,16 @@
+import forca
+import adivinhacao
+
+
 print("*********************************")
-print("***Bem vindo, escolha seu jogo***")
+print("***Bem vindo, escolha seu jogo!***")
 print("*********************************")
+
+print("(1) Forca  (2) Adivinhação")
+
+jogo = int(input("Escolha seu jogo: "))
+
+if (jogo == 1):
+    print("Iniciando FORCA")
+elif (jogo == 2):
+    print("Iniciando ADIVINHAÇÃO")
