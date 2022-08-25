06cdaaacf4ea0f2422a44aa88612bc4d448e5946
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-08-24 22:46:51 -0300

    implementação do nivel de dificuldade

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,8 +6,20 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
-print(numero_secreto)
+total_de_tentativas = 0
+
+print("Qual o nível de dificuldade?")
+print("(1) Fácil  (2) Médio  (3) Difícil")
+
+nivel = int(input("Escolha o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 25
+elif(nivel == 2):
+    total_de_tentativas = 15
+else:
+    total_de_tentativas = 10
+
 
 for rodada in range (1, total_de_tentativas + 1):
     print("tentativa {} de {}".format(rodada, total_de_tentativas))
