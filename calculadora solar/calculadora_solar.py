7706c832a4a2242d5a8fcef4eee6f734dc27db00
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-08-28 19:57:19 -0300

    implementação do laço de repetição

--- calculadora solar/calculadora_solar.py
+++ calculadora solar/calculadora_solar.py
@@ -5,16 +5,21 @@ print("                      ")
 print("***Bem vindo ao calculadora solar!***")
 print("                      ")
 
-
-print("(1) Calcular KhP   (2) Calcular KwH")
+print("(1) Calcular KwP   (2) Calcular KwH")
 calcular = int(input("Escolha a opção que deseja calcular: "))
 
-if (calcular == 1):
-        kwp.Calc()
+while (calcular != 1 or calcular !=2):
+    print("Você selecionou uma opção incorreta")
+    print("(1) Calcular KwP   (2) Calcular KwH")
+    calcular = int(input("Escolha a opção que deseja calcular: "))
 
-else:
-    if (calcular == 2):
-        kwh.calc()
-
-    elif (calcular != 1 or calcular != 2):
-        print("Você selecionou uma opção errada!")
+    if (calcular == 1):
+            kwp.Calc()
+            break
+    else:
+        if (calcular == 2):
+            kwh.calc()
+            break
+        elif (calcular != 1 or calcular != 2):
+            print("Você selecionou uma opção errada!")
+            continue
