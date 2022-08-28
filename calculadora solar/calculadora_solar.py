b9a4f3b20bd27fbddab9d1122f4148b03c0cdc55
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-08-28 18:51:25 -0300

    copia de segurança de código funcional

--- calculadora solar/calculadora_solar.py
+++ calculadora solar/calculadora_solar.py
@@ -5,10 +5,16 @@ print("                      ")
 print("***Bem vindo ao calculadora solar!***")
 print("                      ")
 
+
 print("(1) Calcular KhP   (2) Calcular KwH")
 calcular = int(input("Escolha a opção que deseja calcular: "))
 
 if (calcular == 1):
-    kwp.Calc()
-elif (calcular == 2):
-    kwh.calc()
+        kwp.Calc()
+
+else:
+    if (calcular == 2):
+        kwh.calc()
+
+    elif (calcular != 1 or calcular != 2):
+        print("Você selecionou uma opção errada!")
