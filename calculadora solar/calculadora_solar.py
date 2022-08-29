aa01f87eb476c2787d40886865c5d5f7e0a8f1f8
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-08-29 16:07:34 -0300

    implementação com sucesso de loop

--- calculadora solar/calculadora_solar.py
+++ calculadora solar/calculadora_solar.py
@@ -5,20 +5,20 @@ print("                      ")
 print("***Bem vindo ao calculadora solar!***")
 print("                      ")
 
-print("(1) Calcular KwP   (2) Calcular KwH")
-calcular = int(input("Escolha a opção que deseja calcular: "))
 
-while (calcular != 1 or calcular !=2):
-    print("Você selecionou uma opção incorreta")
+for calcular in range (1000):
     print("(1) Calcular KwP   (2) Calcular KwH")
     calcular = int(input("Escolha a opção que deseja calcular: "))
 
     if (calcular == 1):
-            kwp.Calc()
-            break
+                kwp.Calc()
+                break
+
     else:
         if (calcular == 2):
-            kwh.calc()
-            break
+                kwh.calc()
+                break
+
         elif (calcular != 1 or calcular != 2):
+            print("Você escolheu uma opção incorreta")
             continue
