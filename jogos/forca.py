2a9f2ed79a364e4aad8375eae26f470b6511c4bf
JOSÉ CARLOS LIRA NETO <90408827+jcliraneto@users.noreply.github.com>
2022-09-20 10:13:32 -0300

    update

--- jogos/forca.py
+++ jogos/forca.py
@@ -11,7 +11,15 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
-            print("jogando...")
+
+        chute = input("Qual a letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+        print("Jogando...")
 
     print("fim do jogo! tente outra vez")
 
